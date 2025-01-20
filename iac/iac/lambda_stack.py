9c97bd176c013a1f0e49396b8553000080bcf5e4
mateus <22.01082-3@maua.br>
2025-01-20 19:37:04 -0300

    feat: remove lambda_power_tools

--- iac/iac/lambda_stack.py
+++ iac/iac/lambda_stack.py
@@ -37,7 +37,6 @@ class LambdaStack(Construct):
                                                  compatible_runtimes=[lambda_.Runtime.PYTHON_3_9]
                                                  )
 
-        self.lambda_power_tools = lambda_.LayerVersion.from_layer_version_arn(self, "Lambda_Power_Tools", layer_version_arn="arn:aws:lambda:us-east-2:017000801446:layer:AWSLambdaPowertoolsPythonV2:22")
 
         self.challenge_function = self.create_lambda_api_gateway_integration(
             module_name="challenge_template",
